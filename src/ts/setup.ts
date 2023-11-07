6b1b942646da230465cfa442a04ac16d42ea251c
Michael Makogon <133147452+mmakogon-hl@users.noreply.github.com>
2023-11-07 14:41:48 -0500

    Only add prayer points if unlocked

--- src/ts/setup.ts
+++ src/ts/setup.ts
@@ -2,8 +2,9 @@ export function setup(ctx: Modding.ModContext) {
   ctx.patch(Bank, 'addItem').replace((o, item, quant, logLost, found, ignoreSpace=false, notify=true, itemSource='Game.Unknown') => {
     if(item instanceof BoneItem 
         && item.name === 'Ash' 
-        && game.combat.player.equipment.slots.Amulet.item.name === "Bone Necklace") {
-      game.combat.player.addPrayerPoints(item.prayerPoints * quant * 2);
+        && this.player.equipment.slots.Amulet.item.name === "Bone Necklace" 
+        && game.prayer.isUnlocked) {
+      this.player.addPrayerPoints(item.prayerPoints * quant * 2);
       return true;
     }
     else {
